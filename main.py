cefb06913211816276018ec8a5256a69e9d0058d
cristian-flavius <mateica.flavius@gmail.com>
2025-05-06 06:56:16 +0000

    small update

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py - Fișier de execuție pentru aplicație
-
-import tkinter as tk
-from patient_management import PatientManagementSystem
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = PatientManagementSystem(root)
-    root.mainloop()
